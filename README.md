1bd5139c7ae8a2d1aa5d95004481099f6c6f32b0
br-re <bruno11repic@gmail.com>
2024-11-08 23:23:14 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,41 @@
+
+# TicTacToe Game
+
+Welcome to the TicTacToe Game repository! This project is a simple implementation of the classic TicTacToe game built using React.js. It serves as a practice project for learning and practicing React concepts.
+
+## Overview
+
+TicTacToe is a two-player game where players take turns marking spaces in a 3x3 grid. The player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row wins the game.
+## Features
+
+Play against a friend in real-time.
+Simple and intuitive user interface.
+Responsive design, playable on both desktop and mobile devices.
+## Usage
+
+Clone the repository to your local machine:
+
+bash
+Copy code
+git clone https://github.com/br-re/TicTacToe-Game.git
+Navigate to the project directory:
+
+bash
+Copy code
+cd TicTacToe-Game
+Install dependencies:
+
+Copy code
+npm install
+Start the development server:
+
+sql
+Copy code
+npm start
+Open your web browser and navigate to http://localhost:3000 to play the game.
+## Contributing
+
+Contributions are welcome! If you have any suggestions, bug fixes, or new features to add, feel free to open an issue or create a pull request.
+## License
+
+This project is licensed under the MIT License. You are free to use, modify, and distribute this project for personal or commercial purposes. However, I would appreciate it if you credit me and provide a link to this repository.
---
+++ src/App.jsx
@@ -0,0 +1,123 @@
+import { useState } from "react";
+import Player from "./components/Player.jsx";
+import GameBoard from "./components/GameBoard.jsx";
+import Log from "./components/Log.jsx";
+import GameOver from "./components/GameOver.jsx";
+import { WINNING_COMBINATIONS } from "./winning-combinations.js";
+
+const PLAYERS = {
+  X: "Player 1",
+  O: "Player 2",
+};
+
+const INITAL_GAME_BOARD = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+function deriveActivePlayer(gameTurns) {
+  let currPlayer = "X";
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    currPlayer = "O";
+  }
+
+  return currPlayer;
+}
+function deriveGameBoard(gameTurns) {
+  let gameBoard = [...INITAL_GAME_BOARD.map((array) => [...array])];
+
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+  }
+
+  return gameBoard;
+}
+function deriveWinner(gameBoard, players) {
+  let winner;
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSqaureSymbol =
+      gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol =
+      gameBoard[combination[1].row][combination[1].column];
+    const thridSquareSymbol =
+      gameBoard[combination[2].row][combination[2].column];
+
+    if (
+      firstSqaureSymbol &&
+      firstSqaureSymbol === secondSquareSymbol &&
+      firstSqaureSymbol === thridSquareSymbol
+    ) {
+      winner = players[firstSqaureSymbol];
+    }
+  }
+
+  return winner;
+}
+
+function App() {
+  const [gameTurns, setGameTurns] = useState([]);
+  const [players, setPlayers] = useState(PLAYERS);
+
+  const activePlayer = deriveActivePlayer(gameTurns);
+  const gameBoard = deriveGameBoard(gameTurns);
+  const winner = deriveWinner(gameBoard, players);
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameTurns((prevTurns) => {
+      const currPlayer = deriveActivePlayer(prevTurns);
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: currPlayer },
+        ...prevTurns,
+      ];
+      return updatedTurns;
+    });
+  }
+  
+  let hasDraw = gameTurns.length === 9 && !winner;
+
+  function handleRestart() {
+    setGameTurns([]);
+  }
+
+  function handlePlayerNameChange(symbol, newName) {
+    setPlayers((prevPlayers) => {
+      return {
+        ...prevPlayers,
+        [symbol]: newName,
+      };
+    });
+  }
+
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players" className="highlight-player">
+          <Player
+            initalName={PLAYERS.X}
+            symbol="X"
+            isActive={activePlayer === "X"}
+            onChangeName={handlePlayerNameChange}
+          />
+          <Player
+            initalName={PLAYERS.O}
+            symbol="Y"
+            isActive={activePlayer === "O"}
+            onChangeName={handlePlayerNameChange}
+          />
+        </ol>
+        {(winner || hasDraw) && (
+          <GameOver winner={winner} onRestart={handleRestart} />
+        )}
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
+      </div>
+      <Log turns={gameTurns} />
+    </main>
+  );
+}
+
+export default App;
